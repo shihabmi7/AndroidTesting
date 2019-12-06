7343808da7e2a02b9959d5a23a452c087bb5d3fe
shihabmi7 <shihab.mi7@gmail.com>
2019-12-06 20:38:52 +0600

    Suite Added

---
+++ app/src/androidTest/java/com/framebig/androidtesting/ActivityTestSuite.java
@@ -0,0 +1,10 @@
+package com.framebig.androidtesting;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({HomeActiivtyTest.class, SecondActivity.class})
+public class ActivityTestSuite {
+
+}
--- app/src/androidTest/java/com/framebig/androidtesting/SecondActivityTest.java
+++ app/src/androidTest/java/com/framebig/androidtesting/SecondActivityTest.java
@@ -1,10 +1,25 @@
 package com.framebig.androidtesting;
 
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.runner.AndroidJUnit4;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class SecondActivityTest {
 
+    @Rule
+    public ActivityTestRule<SecondActivity> rule = new ActivityTestRule<>(SecondActivity.class);
+
+    @Test
+    public void haveRecyclerView() {
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()));
+    }
 }
